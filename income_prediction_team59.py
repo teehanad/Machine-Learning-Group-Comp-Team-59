2257d8c9f5ce408658d2ad4dd29369524729a2df
littlethe <56616249+littlethe@users.noreply.github.com>
2019-12-16 23:59:17 +0000

    Final code

--- income_prediction_team59.py
+++ income_prediction_team59.py
@@ -26,68 +26,28 @@ file_name_result    = 'tcd-ml-1920-group-income-submission.csv'
 file_name_fit_processed =       'train-processed.csv'
 file_name_predict_processed =   'test-processed.csv'
 
+def target_merge(data,average,target): # Merging the data.
+    feature = pandas.merge(
+        data.to_frame(data.name),
+        average.reset_index().rename(columns={'index': 'mean', 'mean': 'average'}),
+        on=data.name,
+        how='left'
+    )['average'].rename(data.name + '_mean').fillna(target.mean())
+    feature.index = data.index
+    return feature
 
-def add_noise(series, noise_level):
-    #Fnction to add noise to the series
-    return series * (1 + noise_level * numpy.random.randn(len(series)))
+def target_encoding(fit,predict,target): # The process for target encoding.
 
-def target_encode(dataset,testset,target):
-    #Function to preform Target encoding
-    min_samples_leaf=1 
-    smoothing=1,
-    noise_level=0
-    temp = pandas.concat([dataset, target], axis=1)
+    # Compute target mean
+    temp = pandas.concat([fit, target], axis=1)
+    average = temp.groupby(by=fit.name)[target.name].agg(["mean", "count"])
+    average.drop(["count"], axis=1, inplace=True)
 
-    #Compute target mean 
-    averages = temp.groupby(by=dataset.name)[target.name].agg(["mean", "count"])
+    # Encoding
+    fit = target_merge(fit,average,target)
+    predict = target_merge(predict,average,target)
 
-    #Compute smoothing
-    smoothing = 1 / (1 + numpy.exp(-(averages["count"] - min_samples_leaf) / smoothing))
-
-    #Apply average function to all target data
-    prior = target.mean()
-
-    #The bigger the count the less full_avg is taken into account
-    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
-    averages.drop(["mean", "count"], axis=1, inplace=True)
-
-    # Apply average
-    assert len(dataset) == len(target)
-    assert dataset.name == testset.name
-    temp = pandas.concat([dataset, target], axis=1)
-
-    # Compute target mean 
-    averages = temp.groupby(by=dataset.name)[target.name].agg(["mean", "count"])
-
-    # Compute smoothing
-    smoothing = 1 / (1 + numpy.exp(-(averages["count"] - min_samples_leaf) / smoothing))
-
-    # Apply average function to all target data
-    prior = target.mean()
-
-    # The bigger the count the less full_avg is taken into account
-    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
-    averages.drop(["mean", "count"], axis=1, inplace=True)
-
-    # Apply averages to dataset and testset
-    feature_dataset = pandas.merge(
-        dataset.to_frame(dataset.name),
-        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
-        on=dataset.name,
-        how='left')['average'].rename(dataset.name + '_mean').fillna(prior)
-
-    # pandas.merge does not keep the index so restore it
-    feature_dataset.index = dataset.index 
-    feature_testset = pandas.merge(
-        testset.to_frame(testset.name),
-        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
-        on=testset.name,
-        how='left')['average'].rename(dataset.name + '_mean').fillna(prior)
-
-    # pd.merge does not keep the index so restore it
-    feature_testset.index = testset.index
-
-    return add_noise(feature_dataset, noise_level), add_noise(feature_testset, noise_level)
+    return fit,predict
 
 def checkUnique(filetofix): #Helper function made by Adam
     print("Year of Record", filetofix["Year of Record"].unique())
@@ -129,6 +89,8 @@ def printInfo(file_fit,file_predict): #Helper function made by Adam
     print("---------------------------------------------------------------------------------------------------ADAM'S NOTES----------------------------------------------------------------------------------------------------")
     print(""" 
     Set debug = 1 if you wish to see info about imported data printed, 0 if you do not want that \n
+    I am not sure what crime rate is measured in terms of and what the upper and lower bounds of that scale are, seems fairly arbitrary to me \n
+    Unknown in hair color makes sense as people can be bald so I shall leave that \n
     """)
     print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
     if debug == 1:   
@@ -166,56 +128,40 @@ def preprocess(filetofix, filename): #Adam's code
     filetofix["Yearly Income in addition to Salary (e.g. Rental Income)"] = pandas.to_numeric(filetofix["Yearly Income in addition to Salary (e.g. Rental Income)"])
 
     filetofix['Year of Record'].fillna(filetofix['Year of Record'].median(), inplace=True)  # Use median to replace of 'NA'. By Lin 2019/11/10
-    
-    #Replace NA and 0 with None
+
     filetofix["Housing Situation"] = filetofix["Housing Situation"].replace( "nA" ,'None')
     filetofix["Housing Situation"] = filetofix["Housing Situation"].replace( "0" ,'None')
 
-    #Replace #NUM with a numpy nan
     filetofix["Work Experience in Current Job [years]"] = filetofix["Work Experience in Current Job [years]"].replace( "#NUM!" ,numpy.NaN)
-    #print('median:',filetofix['Work Experience in Current Job [years]'].median())
-    #filetofix['Work Experience in Current Job [years]'].fillna(filetofix['Work Experience in Current Job [years]'].median(), inplace=True)
-    #Replace all numpy nan's with a numeric 0
     filetofix['Work Experience in Current Job [years]'].fillna(0, inplace=True)
-    #change column datatype to float64
     filetofix["Work Experience in Current Job [years]"] = pandas.to_numeric(filetofix["Work Experience in Current Job [years]"])
 
-    #Replace all f, 0, nan, unknown, #N/A with a numnpy nan
     filetofix["Gender"] = filetofix["Gender"].replace( "f" ,'female')
     filetofix["Gender"] = filetofix["Gender"].replace( "0" ,'other')
     filetofix["Gender"] = filetofix["Gender"].replace( "nan" ,numpy.NaN)
     filetofix["Gender"] = filetofix["Gender"].replace( 'unknown' ,numpy.NaN)
     filetofix["Gender"] = filetofix["Gender"].replace( "#N/A" ,numpy.NaN)
-    #Replae all numpy nan with the mode of the cloumn
     filetofix['Gender'].fillna(filetofix['Gender'].mode(dropna=True)[0], inplace=True) 
 
-    #replace nan with numpy nan
     filetofix["Satisfation with employer"] = filetofix["Satisfation with employer"].replace( "nan" ,numpy.NaN)
-    replace numpy nan with mode of column
     filetofix['Satisfation with employer'].fillna(filetofix['Satisfation with employer'].mode(dropna=True)[0], inplace=True) 
 
-    
+
     filetofix["Country"] = filetofix["Country"].replace( '0' ,'Unknown')
-    
-    #repalce #N/A. numpy nan, 0, no and Non with None
+
     filetofix["University Degree"] = filetofix["University Degree"].replace( "#N/A" ,'None')
     filetofix["University Degree"] = filetofix["University Degree"].replace( numpy.NaN ,'None')
     filetofix["University Degree"] = filetofix["University Degree"].replace( "0" ,'None')
     filetofix["University Degree"] = filetofix["University Degree"].replace( "no" ,'None')
     filetofix["University Degree"] = filetofix["University Degree"].replace( "No" ,'None')
 
-    #Replace 0 and numpy nan with Unknown
     filetofix["Hair Color"] = filetofix["Hair Color"].replace( '0' ,'Unknown')
     filetofix["Hair Color"] = filetofix["Hair Color"].replace( numpy.NaN ,'Unknown')
 
-    #replace numpy nan with mode of column
     filetofix['Profession'].fillna(filetofix['Profession'].mode(dropna=True)[0], inplace=True)
-    
-    #replace numpy nan with mode of column
     filetofix['Country'].fillna(filetofix['Country'].mode(dropna=True)[0], inplace=True) 
 
-    #removed_columns = ['Crime Level in the City of Employement', 'Size of City','Wears Glasses','Yearly Income in addition to Salary (e.g. Rental Income)']
-    #removed_columns = ['Crime Level in the City of Employement', 'Hair Color']
+    # Those columns are not used.
     removed_columns = ['Age','Hair Color','Wears Glasses','Body Height [cm]']
     for column in removed_columns:
         del filetofix[column]
@@ -226,7 +172,6 @@ def encoding(file_fit,file_predict):         #Deepthi's code
     #One hot encoding is here, because Deepthi doesn't know how to put her code in this function, therefore I modified her code and put here. By Lin 2019/11/9
     print('Start one hot encoding.')
     # The columns used to one hot encoding.
-    #columns = ['Gender','Housing Situation','Hair Color','University Degree','Satisfation with employer']
     columns = []
     for column in columns:
         if column in file_fit.columns:
@@ -240,39 +185,28 @@ def encoding(file_fit,file_predict):         #Deepthi's code
     print('Start target encoding.')
     for column in columns:
         if column in file_fit.columns:
-            file_fit[column],file_predict[column] = target_encode(file_fit[column],file_predict[column],target)
+            file_fit[column],file_predict[column] = target_encoding(file_fit[column],file_predict[column],target)
     print('Target encoding is finished.')
     return file_fit,file_predict
 
-def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code updated:2019/11/10
-
-##    print('Output the processed files.')
-##    file_fit.to_csv(file_name_fit_processed,index=False)
-##    file_predict.to_csv(file_name_predict_processed,index=False)
-
-##    print('Check income.')
-##    null_columns=file_fit.columns[file_fit.isnull().any()]
-##    print(file_fit[file_fit["Total Yearly Income [EUR]"].isnull()][null_columns])
+# Analysis: file_fit is training data, file_predict is testing data. model_no is selected model, there are 4 models we can use.
+# test_mode is true or false, true is calculating score, false is creating file for Kaggle.
+def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code updated:2019/11/26
 
     fit_y = file_fit['Total Yearly Income [EUR]']
 
     #The columns are not used temporarily.
-    #unused_columns = ['Total Yearly Income [EUR]','Satisfation with employer','Country','Profession']
     unused_columns = ['Total Yearly Income [EUR]']
     for column in unused_columns:
         file_fit = file_fit.drop([column],axis=1)
         file_predict = file_predict.drop([column],axis=1)
-
-    
-##    improved_columns = ['Country','Yearly Income in addition to Salary (e.g. Rental Income)']
-##    for column in improved_columns:
-##        file_fit[column] = pow(file_fit[column],0.5)
     
     # Do not include instance.
     fit_x = file_fit.iloc[:,1:]
     predict_x = file_predict.iloc[:,1:]
 
-
+    # If it is in test mode, the 20% training data will be used to be local testing data for calculating score,
+    # therefore we can understand how accurate it is without submitting.
     if(test_mode):
         fit_x,predict_x,fit_y,predict_y = train_test_split(fit_x.values, fit_y.values, test_size=0.2, random_state=30)
     else:
@@ -280,14 +214,17 @@ def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code upda
         fit_y = fit_y.values
 
 
+    # Option 0: Using linear regression and polynomialFeatures.
     if (model_no == 0):
         print('Using linear regression')
-        model = make_pipeline(PolynomialFeatures(1),linear_model.LinearRegression()) #using PolynomialFeatures
+        model = make_pipeline(PolynomialFeatures(1),linear_model.LinearRegression()) #Using PolynomialFeatures
         print('Start fitting.')
         model.fit(fit_x,fit_y)
         print('Fitting is finished, start predicting.')
         result = model.predict(predict_x)
         print('Predicting is finished.')
+
+    # Option 1: Using LightGBM.   
     elif(model_no == 1):
         print('Using LightGBM')
         train_x, test_x, train_y, test_y = train_test_split(fit_x, fit_y, test_size=0.2, random_state=42)
@@ -321,6 +258,7 @@ def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code upda
         
         result = model.predict(predict_x)
 
+    # Option 2: Using RandomForest.  
     elif(model_no == 2):
         print("Using RandomForest")
         randomForestGenerator = RandomForestRegressor(n_estimators=200,n_jobs=-1)
@@ -332,6 +270,7 @@ def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code upda
         print("Starting prediction")
         result = randomForestGenerator.predict(predict_x)
 
+    # Option 3: Using BayesianRidge.
     elif(model_no == 3):
         print('Using BayesianRidge')
         model = BayesianRidge()
@@ -339,11 +278,12 @@ def analysis(file_fit,file_predict,model_no,test_mode):         #Lin's code upda
         result = model.predict(predict_x)
         
 
-        
+    # If it is in test mode, it will calculate score. Less score is better.
     if(test_mode):
         score = result-predict_y
         score = abs(score)
         print('score:',sum(score))
+    # If it is not in test mode, it will create the file for Kaggle.
     else:   
         # Output
         print('Output the files.')
@@ -367,13 +307,16 @@ def main():
 
     # Calling our functions.
     printInfo(file_fit, file_predict)
-    
+
+    # Preprocess, Adam wrote it.
     file_fit = preprocess(file_fit, "FILE_FIT")
     file_predict = preprocess(file_predict, "FILE_PREDICT")
-    
+
+    # Encoding, Deepthi wrote it.
     file_fit,file_predict = encoding(file_fit,file_predict)
 
-    analysis(file_fit,file_predict,2,False)
+    # Analysis, Lin wrote it.
+    analysis(file_fit,file_predict,2,True)
 
     print('Finished...')
 
